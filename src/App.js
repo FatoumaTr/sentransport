a4294fe309d941319aa21c5cf4c6ae4836e69cfd
Fatoumata Bintou Traore <fimatraore24@gmail.com>
2026-04-25 00:23:09 +0000

    Ajout Footer, date Header et composant Statistique

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import './App.css';
 import Header from './Header';
-function App () {
-return (
-< div className = " App " >
-< Header / >
-< main className = " contenu " >
-<p > Bienvenue ! Cette application vous aide a trouver
-votre ligne de bus a Dakar . </p >
-</ main >
-</ div >
-);
+import Footer from './Footer';
+import Statistique from './Statistique';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="contenu">
+        <p>Bienvenue ! Cette application vous aide a trouver
+        votre ligne de bus a Dakar.</p>
+        <div className="stats">
+          <Statistique />
+          <Statistique />
+          <Statistique />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
-export default App ;
+
+export default App;
